8642e1bedf5385e83c12c70a7b1205c62ea5ea27
mavuong3112 <160447834+mavuong3112@users.noreply.github.com>
2025-12-01 23:24:46 +0700

    Update AssemblyInfo.cs

--- Code/DATALAYER/Properties/AssemblyInfo.cs
+++ Code/DATALAYER/Properties/AssemblyInfo.cs
@@ -1,9 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// associated with an assembly
 [assembly: AssemblyTitle("DATALAYER")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
